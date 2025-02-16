997e1feb749251b6916ba543d6fad93779573f2a
Adam Elsayed <adamelsayyed06@gmail.com>
2025-02-16 13:10:59 -0500

    added flask endpoints + config file

--- main.py
+++ main.py
@@ -1,11 +1,72 @@
 from twitter_client import TwitterClient
 from tweet_processor import TweetProcessor
+import config
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/')
+def home():
+    return []
+
+@app.route('/get_tweets')
+def get_tweets():
+    api = TwitterClient()
+    tweet_processor = TweetProcessor()
+
+    search = request.args.get('query')
+    tweets = api.get_tweets(search)
+
+    if not tweets:
+        return jsonify #error message
+    
+    analyzed_tweets = [{}]
+    positiveTweets = {}
+    negativeTweets = {}
+
+    for tweet in tweets:
+        analyzed_tweets[tweet] = tweet_processor.get_tweet_sentiment(tweet)
+
+    for analyzed_tweet in analyzed_tweets:
+        if(analyzed_tweets[analyzed_tweet] > 0):
+            positiveTweets.update({analyzed_tweet : 'positive'})
+        else:
+            negativeTweets.update({analyzed_tweet : 'negative'})
+
+    response = {**positiveTweets, **negativeTweets}
+
+    return jsonify(response)
+
+@app.route('get_tweets_score')
+def get_tweets_score():
+    api = TwitterClient()
+    tweet_processor = TweetProcessor()
+
+    search = request.args.get('query')
+    tweets = api.get_tweets(search)
+
+    averageScore = 0
+
+    for tweet in tweets:
+        averageScore += tweet_processor.get_tweet_sentiment(tweet)
+    averageScore *= 10
+
+    return{ 'averageScore' : averageScore }
+
+
+        
+    
+
+
+
 
 def main():
     api = TwitterClient() #creating an instance of the TwitterClient class
     tweet_processor = TweetProcessor() #creating an instance of the TweetProcessor class
 
     tweets = api.get_tweets("Donald Trump") #query "Donald Trump"
+
 
     if not tweets: #if no tweets are fetched
         print("No tweets found")
